894ede48ad7ef8f2d0eec927d418931a04b3ab38
Abhinav <bpushpa973@gmail.com>
2026-02-26 21:09:01 +0530

    Update connect.py

--- CLONNE_MUSIC/plugins/tools/connect.py
+++ CLONNE_MUSIC/plugins/tools/connect.py
@@ -7,6 +7,9 @@ login_sessions = {}
 @app.on_message(filters.command("connect") & filters.private)
 async def connect_assistant(client, message):
 
+    bot = await client.get_me()
+    bot_id = bot.id
+
     if len(message.command) < 2:
         return await message.reply_text("Usage:\n/connect +91XXXXXXXXXX")
 
@@ -15,7 +18,7 @@ async def connect_assistant(client, message):
     msg = await message.reply_text("📲 Sending OTP...")
 
     assistant = Client(
-        name=f"assistant_login_{message.from_user.id}",
+        name=f"assistant_login_{bot_id}",
         api_id=API_ID,
         api_hash=API_HASH,
         device_model="CLONNE_MUSIC Assistant",
@@ -25,16 +28,12 @@ async def connect_assistant(client, message):
 
     await assistant.connect()
 
-    try:
-        sent_code = await assistant.send_code(phone)
+    sent_code = await assistant.send_code(phone)
 
-        login_sessions[message.from_user.id] = {
-            "client": assistant,
-            "phone": phone,
-            "phone_code_hash": sent_code.phone_code_hash
-        }
+    login_sessions[bot_id] = {
+        "client": assistant,
+        "phone": phone,
+        "phone_code_hash": sent_code.phone_code_hash
+    }
 
-        await msg.edit("✅ OTP Sent!\n\nSend OTP like:\n`/otp 1 2 3 4 5`")
-
-    except Exception as e:
-        await msg.edit(f"❌ Error:\n{e}")
+    await msg.edit("OTP sent ✅\nSend:\n/otp 1 2 3 4 5")
