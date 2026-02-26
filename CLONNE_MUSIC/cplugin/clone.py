2dcf33316e916ca82d61c94c67d1581912d876ec
Abhinav <bpushpa973@gmail.com>
2026-02-26 14:18:10 +0530

    Add files via upload

---
+++ CLONNE_MUSIC/cplugin/clone.py
@@ -0,0 +1,31 @@
+import time
+import random
+from datetime import datetime
+
+import psutil
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+
+from CLONNE_MUSIC import app
+from config import PING_IMG_URL, CLONNE_PICS, BOT_USERNAME
+from .utils import StartTime
+from CLONNE_MUSIC.utils import get_readable_time
+from CLONNE_MUSIC.utils.decorators.language import language
+
+APP_LINK = f"https://t.me/{BOT_USERNAME}"
+
+
+@Client.on_message(filters.command("clone"))
+@language
+async def ping_clone(client: Client, message: Message, _):
+    bot = await client.get_me()
+
+
+    hmm = await message.reply_photo(
+        photo=random.choice(CLONNE_PICS), caption=_["NO_CLONE_MSG"],
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [InlineKeyboardButton("Cʟᴏɴᴇ Bᴏᴛ", url=APP_LINK)]
+            ]
+        )
+    )
