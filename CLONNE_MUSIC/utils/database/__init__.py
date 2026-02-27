2a74bcd516e81d6b94b84f38cdf5dbb2a848fb76
Abhinav <bpushpa973@gmail.com>
2026-02-27 14:53:03 +0530

    Update __init__.py

--- CLONNE_MUSIC/utils/database/__init__.py
+++ CLONNE_MUSIC/utils/database/__init__.py
@@ -1,2 +1,8 @@
 from .database import *
-from .clonedb import *
+from motor.motor_asyncio import AsyncIOMotorClient
+from config import MONGO_DB_URI
+
+mongo = AsyncIOMotorClient(MONGO_DB_URI)
+db = mongo.CLONNE_MUSIC
+
+clonebotdb = db.clonebots
