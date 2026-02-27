749cf32db2a9f5c226e60cc77cb29d65700bc7e4
Abhinav <bpushpa973@gmail.com>
2026-02-27 16:47:41 +0530

    Update __init__.py

--- CLONNE_MUSIC/utils/database/__init__.py
+++ CLONNE_MUSIC/utils/database/__init__.py
@@ -5,3 +5,15 @@ mongo = AsyncIOMotorClient(MONGO_DB_URI)
 db = mongo.CLONNE_MUSIC
 
 clonebotdb = db.clonebots
+cmode = db.cmode
+async def get_cmode(chat_id):
+    data = cmode.find_one({"chat_id": chat_id})
+    return data.get("mode") if data else None
+
+
+async def set_cmode(chat_id, mode):
+    cmode.update_one(
+        {"chat_id": chat_id},
+        {"$set": {"mode": mode}},
+        upsert=True
+    )
